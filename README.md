eceb7456071b83d1c67f656190b2a6374eba99a6
Anubhav <mr.anubhavtiwary@gmail.com>
2024-10-17 17:32:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Algorithm Visualizer
+
+A better visualization of different algorithms made with React.
+
+There are 6 segments
+
+-   Pathfinder
+-   Prime Numbers
+-   Sorting Algorithms
+-   N Queen
+-   Binary Search Game
+-   Binary Tree Traversals
+
+## Algorithms implemented
+
+-   Graph Search
+    -   DFS
+    -   BFS
+    -   Dijkstra
+    -   Maze Creation
+-   Sorting
+    -   Bubble sort
+    -   Selection sort
+    -   Insertion sort
+    -   Heap sort
+    -   Merge sort
+    -   Quick sort
+-   Sieve of Eratosthenes
+-   N Queen Backtracking
+-   Binary Search
+-   Binary Tree Traversals
+    -   Inorder Traversals
+    -   Preorder Traversals
+    -   Postorder sort
---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Pathfinder from "./pathfinderComponents/pathfinder";
+import Home from "./homeComponents/home";
+import Seive from "./primeComponents/seive";
+import Sort from "./sortComponents/sort";
+import Queen from "./queenComponents/queen";
+import BinarySearch from "./binarySearchComponent/binarySearch";
+import RecursiveSort from "./recursiveSortComponents/recursiveSort";
+import Tree from "./treeComponents/Tree"
+
+function App() {
+		return (
+			<Router basename='/'>
+				<Switch>
+					<Route path='/pathfinder' component={Pathfinder} />
+					<Route path='/prime' component={Seive} />
+					<Route path='/sort' component={Sort} />
+					<Route path='/nqueen' component={Queen} />
+					<Route path='/binarysearch' component={BinarySearch} />
+					<Route path='/recursivesort' component={RecursiveSort} />
+					<Route path='/tree' component={Tree} />
+					<Route path='/' component={Home} />
+				</Switch>
+			</Router>
+		);
+	}
+
+export default App;
