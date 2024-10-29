59cb50c1bdb282f6324cf9788f93d05d706f1735
Anubhav <mr.anubhavtiwary@gmail.com>
2024-10-29 12:17:15 +0530

    added n-queen

---
+++ frontend/src/queenComponents/cells.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Cell from "./cell";
+class Cells extends Component {
+	render() {
+		return (
+			<div className='booard m-5'>
+				{this.props.board.map((row, rowidx) => {
+					return (
+						<div key={rowidx}>
+							{row.map((cell, cellidx) => {
+								return <Cell key={cellidx} cell={cell} />;
+							})}
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
+}
+
+export default Cells;
